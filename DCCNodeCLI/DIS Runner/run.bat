15deda91c068269734377e4e56d9fa23c14d11f2
Veaceslav BARBARII <slava.barbarii@gmail.com>
2017-11-18 19:27:55 +0200

    Change DCCNodeCLI Structure

---
+++ DCCNodeCLI/DIS Runner/run.bat
@@ -0,0 +1,9 @@
+@echo off
+title Distributed Information Systems
+echo Distributed Information Systems is launched
+start /b cmd /c DCCN.exe -i 1 > nul
+start /b cmd /c DCCN.exe -i 2 > nul
+start /b cmd /c DCCN.exe -i 3 > nul
+start /b cmd /c DCCN.exe -i 4 > nul
+start /b cmd /c DCCN.exe -i 5 > nul
+start /b cmd /c DCCN.exe -i 6 > nul
--- DCCNodeCLI/Data/Actual Schemas/Employees.Designer.cs
+++ DCCNodeCLI/Data/Actual Schemas/Employees.Designer.cs
@@ -10,7 +10,7 @@
 
 #pragma warning disable 1591
 
-namespace DCCClientLib.Schemas {
+namespace DCCNodeCLI.Data.Actual_Schemas {
     
     
     /// <summary>
